f18768274d3de2050b9d766393392341007de687
wyatt-drew <wyatt.j.drew@gmail.com>
2024-07-02 21:11:25 -0600

    Deploy

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Hero from './components/Hero/Hero';
 import Navbar from './components/Navbar/Navbar';
 import Footer from './components/Footer/Footer';
